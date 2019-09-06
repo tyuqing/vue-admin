17c2abf41d5ed644ae40a96e04ac2b2bdeea742a
tyuqing <tyuqing7@163.com>
2019-09-06 17:09:05 +0800

    feat: import the basic component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import iview from 'iview';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false;
+Vue.use(iview);
 
 new Vue({
   router,
