0dc193c18efa589698d75d6b13fc15af99e252f4
tyuqing <tyuqing7@163.com>
2019-09-06 17:10:16 +0800

    feat(route): the constant route, such as login dashboard

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Layout from '@/layout/index.vue';
+
+Vue.use(Router);
+
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index.vue'),
+    hidden: true,
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index.vue'),
+        name: 'dashboard',
+        meta: { title: '首页' },
+      },
+    ],
+  },
+];
+export default new Router({
+  routes: constantRoutes,
+});
+/**
+ * TODO重置路由
+ */
+export function resetRouter() {
+
+}
